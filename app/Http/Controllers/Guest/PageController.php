e6884e2f30e6f0f2b40fb2c771fc68230bcf9e33
post-coder <gabrielspanu.dev@gmail.com>
2024-04-09 09:57:05 +0200

    feat: controller created

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+// serve per non dover ogni volta inserire i percorsi degli eventuali altri elementi che vorremmo importare qui dentro
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    
+    
+
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
 
+<div class="container py-5">
+
+    <h1>Ciao a tutti</h1>
+</div>
+
+
 @endsection
